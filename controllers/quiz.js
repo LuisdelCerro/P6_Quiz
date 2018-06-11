cfa4d20df4c1a336f99c96114d5d08505337f7f5
LuisdelCerro <36802632+LuisdelCerro@users.noreply.github.com>
2018-06-11 03:21:14 +0200

    Step 10 - Heroku Postgres

---
+++ controllers/quiz.js
@@ -0,0 +1,206 @@
+const Sequelize = require("sequelize");
+const {models} = require("../models");
+
+// Autoload the quiz with id equals to :quizId
+exports.load = (req, res, next, quizId) => {
+
+    models.quiz.findById(quizId)
+    .then(quiz => {
+        if (quiz) {
+            req.quiz = quiz;
+            next();
+        } else {
+            throw new Error('There is no quiz with id=' + quizId);
+        }
+    })
+    .catch(error => next(error));
+};
+
+
+// GET /quizzes
+exports.index = (req, res, next) => {
+
+    models.quiz.findAll()
+    .then(quizzes => {
+        res.render('quizzes/index.ejs', {quizzes});
+    })
+    .catch(error => next(error));
+};
+
+
+// GET /quizzes/:quizId
+exports.show = (req, res, next) => {
+
+    const {quiz} = req;
+
+    res.render('quizzes/show', {quiz});
+};
+
+
+// GET /quizzes/new
+exports.new = (req, res, next) => {
+
+    const quiz = {
+        question: "", 
+        answer: ""
+    };
+
+    res.render('quizzes/new', {quiz});
+};
+
+// POST /quizzes/create
+exports.create = (req, res, next) => {
+
+    const {question, answer} = req.body;
+
+    const quiz = models.quiz.build({
+        question,
+        answer
+    });
+
+    // Saves only the fields question and answer into the DDBB
+    quiz.save({fields: ["question", "answer"]})
+    .then(quiz => {
+        req.flash('success', 'Quiz created successfully.');
+        res.redirect('/quizzes/' + quiz.id);
+    })
+    .catch(Sequelize.ValidationError, error => {
+        req.flash('error', 'There are errors in the form:');
+        error.errors.forEach(({message}) => req.flash('error', message));
+        res.render('quizzes/new', {quiz});
+    })
+    .catch(error => {
+        req.flash('error', 'Error creating a new Quiz: ' + error.message);
+        next(error);
+    });
+};
+
+
+// GET /quizzes/:quizId/edit
+exports.edit = (req, res, next) => {
+
+    const {quiz} = req;
+
+    res.render('quizzes/edit', {quiz});
+};
+
+
+// PUT /quizzes/:quizId
+exports.update = (req, res, next) => {
+
+    const {quiz, body} = req;
+
+    quiz.question = body.question;
+    quiz.answer = body.answer;
+
+    quiz.save({fields: ["question", "answer"]})
+    .then(quiz => {
+        req.flash('success', 'Quiz edited successfully.');
+        res.redirect('/quizzes/' + quiz.id);
+    })
+    .catch(Sequelize.ValidationError, error => {
+        req.flash('error', 'There are errors in the form:');
+        error.errors.forEach(({message}) => req.flash('error', message));
+        res.render('quizzes/edit', {quiz});
+    })
+    .catch(error => {
+        req.flash('error', 'Error editing the Quiz: ' + error.message);
+        next(error);
+    });
+};
+
+
+// DELETE /quizzes/:quizId
+exports.destroy = (req, res, next) => {
+
+    req.quiz.destroy()
+    .then(() => {
+        req.flash('success', 'Quiz deleted successfully.');
+        res.redirect('/quizzes');
+    })
+    .catch(error => {
+        req.flash('error', 'Error deleting the Quiz: ' + error.message);
+        next(error);
+    });
+};
+
+
+// GET /quizzes/:quizId/play
+exports.play = (req, res, next) => {
+
+    const {quiz, query} = req;
+
+    const answer = query.answer || '';
+
+    res.render('quizzes/play', {
+        quiz,
+        answer
+    });
+};
+
+exports.randomPlay = (req,res,next) => {
+
+if(req.session.randomPlay===undefined){
+    req.session.randomPlay=[];
+    }
+        const whereOpt={"id":{[Sequelize.Op.notIn]:req.session.randomplay}};
+        return models.quiz.findAll({where:whereOpt})
+    .then(quizzes=>{
+
+     if(quizzes.length()===0){
+         let score=req.session.randomPlay.length;
+         req.session.randomPlay=[];
+         res.render('random_nomore', {
+             score
+         });
+     }
+
+     let id_azar = Math.floor(Math.random()*quizzes.length());
+     req.session.randomplay[req.sesion.randomplay.length]=id_azar;
+     let quiz=quizzes[id_azar];
+     let score=req.session.randomPlay.length;
+     res.render('random_play', {
+            score,
+            quiz
+        });
+    });
+}
+
+// GET /quizzes/:quizId/check
+exports.check = (req, res, next) => {
+
+    const {quiz, query} = req;
+
+    const answer = query.answer || "";
+    const result = answer.toLowerCase().trim() === quiz.answer.toLowerCase().trim();
+
+    res.render('quizzes/result', {
+        quiz,
+        result,
+        answer
+    });
+
+    exports.randomCheck = (req,res,next) => {
+
+        const {quiz, query} = req;
+        const answer = query.answer || "";
+        const result = answer.toLowerCase().trim() === quiz.answer.toLowerCase().trim();
+        let score=req.session.randomPlay.length;
+        if(!result){
+        let score=req.session.randomPlay.length-1;
+        req.session.randomPlay=[];
+        }
+        else{
+        let score=req.session.randomPlay.length;
+        }
+      res.render('randomResult',{
+          score,
+          quiz,
+          result,
+          answer,
+
+      })
+
+
+    }
+};
